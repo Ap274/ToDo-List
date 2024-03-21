0f573be94180b0c36bd75d9f3cbbdc811b9d5ccb
Ap274 <aldopiresaps@gmail.com>
2024-03-21 15:46:01 -0300

    feat: creating basic project structure

---
+++ fonts.ts
@@ -0,0 +1,10 @@
+export const fontFamily = {
+    regular: 'Inter_400Regular',
+    bold: 'Inter_700Bold',
+}
+
+export const fontSize = {
+    sm: 12,
+    md: 14,
+    lg: 16,
+}
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native"
+
+import { colors } from "../../../colors"
+
+export const styles = StyleSheet.create({
+    container: {
+        height: 173,
+        backgroundColor: colors.gray700,
+    }, 
+})
